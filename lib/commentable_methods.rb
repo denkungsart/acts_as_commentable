05dfad8fed6dcbbbc37122bbdcff525ef52f0a4e
Fabian Schwahn <fabian.schwahn@gmail.com>
2021-09-07 08:52:21 +0200

    Add :inverse_of to has_many_options

--- lib/commentable_methods.rb
+++ lib/commentable_methods.rb
@@ -38,6 +38,7 @@ module Juixe
         def has_many_options(role)
           {:class_name => "Comment",
                   :as => :commentable,
+                  :inverse_of => :commentable,
                   :dependent => :destroy,
                   :before_add => Proc.new { |x, c| c.role = role.to_s }
           }
@@ -62,9 +63,9 @@ module Juixe
             comment_roles.each do |role|
               define_role_based_inflection(role)
             end
-            has_many :all_comments, { :as => :commentable, :dependent => :destroy, class_name: 'Comment' }.merge(join_options)
+            has_many :all_comments, { :as => :commentable, :inverse_of => :commentable, :dependent => :destroy, class_name: 'Comment' }.merge(join_options)
           else
-            has_many :comments, {:as => :commentable, :dependent => :destroy}.merge(join_options)
+            has_many :comments, {:as => :commentable, :inverse_of => :commentable, :dependent => :destroy}.merge(join_options)
           end
 
           comment_types.each do |role|
